89140e20916807d460e6acc5c31917bcc8379a66
Zuha Aqib <117381907+z-aqib@users.noreply.github.com>
2025-12-01 15:58:58 +0500

    added a wrapper class which wraps RAG and ML seperately for user calling

---
+++ src/ml/service.py
@@ -0,0 +1,7 @@
+from src.ml.recommenders.item_item import ItemItemRecommender
+
+model = ItemItemRecommender("data/processed").fit()
+
+
+def get_ml_candidates_for_user(user_id: str, k=5):
+    return model.recommend_for_user(user_id=user_id, k=k, exclude_seen=True)
---
+++ src/rag/rag_service.py
@@ -0,0 +1,2 @@
+# from rag_with_gemini_api import ask
+# Wrapper to expose ask() function for RAG service
