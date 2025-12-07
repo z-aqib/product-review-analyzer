153a1bf5202344e5904bb6b2b19f396beb879ff5
Zuha Aqib <117381907+z-aqib@users.noreply.github.com>
2025-12-07 22:43:23 +0500

    commented out this test as it was just a test file

--- src/llm/experiments/test_run_hf.py
+++ src/llm/experiments/test_run_hf.py
@@ -3,5 +3,5 @@ from gradio_client import Client
 
 client = Client("MuhammadHaaris/mlops")
 print("Client created")
-result = client.predict(user_input="Hello!!", api_name="/predict")
-print("Result:", result)
+# result = client.predict(user_input="Hello!!", api_name="/predict")
+# print("Result:", result)
